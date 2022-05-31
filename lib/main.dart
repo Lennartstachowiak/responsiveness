a9087360071a119b9c28e8d4845d88b435190482
Lennartstachowiak <Lennart.Stachowiak@code.berlin>
2022-05-31 16:19:42 +0200

    feat: create big and small view

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:responsiveness/data.dart';
-import 'package:responsiveness/widgets/widgets.dart';
+import 'package:responsiveness/view/big_view.dart';
+import 'package:responsiveness/view/small_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -23,51 +23,15 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Builder(builder: (context) {
-        return Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: ListView.builder(
-            itemCount: emails.length,
-            itemBuilder: (context, index) {
-              final email = emails[index];
-              return InkWell(
-                onTap: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) => EmailDetails(email: email),
-                    ),
-                  );
-                },
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 8.0),
-                  child: Row(
-                    children: [
-                      SenderAvatar(sender: email.sender),
-                      Expanded(
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: EmailPreview(email: email),
-                        ),
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        children: [
-                          EmailDeliveryInfo(email: email),
-                          IconButton(
-                            onPressed: () {},
-                            icon: const Icon(Icons.star_border),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-              );
-            },
-          ),
-        );
-      }),
+    return LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints constraints) {
+        if (constraints.maxWidth <= 600 ) {
+          return const SmallView();
+        } else {
+          return const BigView();
+        }
+        
+      },
     );
   }
 }
